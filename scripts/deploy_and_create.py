91b02858410b53b2218dcfe22860f724f4ae56ed
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-09 17:43:47 +0900

    sample token uri added

---
+++ scripts/deploy_and_create.py
@@ -0,0 +1,8 @@
+from scripts.helpful_scripts import get_account, simpleCollectible
+
+sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+
+
+def main():
+    account = get_account()
+    simple_collectible = simpleCollectible.deploy({"from": account})
