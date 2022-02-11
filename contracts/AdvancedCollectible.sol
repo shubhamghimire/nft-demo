ebc5f14e213975e9bab288b6e3ba4b0d714a75ee
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-11 19:59:42 +0900

    AdvancedCollectible contract created and brownie config updated

---
+++ contracts/AdvancedCollectible.sol
@@ -0,0 +1,14 @@
+// An NFT contract 
+// Where the tokenURI is one of different gods
+// Randomly Selected
+
+/ SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol"
+
+contract AdvancedCollectibel is ERC721 {
+
+}
