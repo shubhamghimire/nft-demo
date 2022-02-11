d7b354b5f29f818e7592c7cf8714d78963654776
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-11 23:14:02 +0900

    AdvancedCollectible updated

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -2,13 +2,25 @@
 // Where the tokenURI is one of different gods
 // Randomly Selected
 
-/ SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol"
 
-contract AdvancedCollectibel is ERC721 {
+contract AdvancedCollectibel is ERC721, VRFConsumerBase {
+    uint256 public tokenCounter;
+    bytes32 public keyhash;
+    uint256 public fee;
 
+    constructor(address _vrfCoordinator, address _linkToken, bytes32 _keyhash, uint256 fee) public
+    VRFConsumerBase(_vrfCoordinator, _linkToken)
+    ERC721("Power", "GOD") {
+        tokenCounter = 0;
+        fee = _fee;
+    }
+
+    function createCollectible(string memory tokenURI) public returns (bytes32)
+    
 }
