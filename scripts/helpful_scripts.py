9f5dde4093143465e5ac590a5f55c16ad85f2891
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-13 13:29:46 +0900

    get_contract function added in helpful_scripts.py

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,5 @@
-from brownie import accounts, network, config
+from brownie import accounts, network, config, LinkToken, VRFCoordinatorMock, Contract
+from web3 import Web3
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = [
     "development",
@@ -19,3 +20,34 @@ def get_account(index=None, id=None):
     if id:
         return accounts.load(id)
     return accounts.add(config["wallets"]["from_key"])
+
+
+contract_to_mock = {"link_token": LinkToken, "vrf_coordinator": VRFCoordinatorMock}
+
+
+def get_contract(contract_name):
+    """
+    This function will either:
+        - Get an address from the config
+        - Or deploy a Mock to use for a network that doesn't have the contract
+    Args:
+        contract_name (string): This is the name of the contract that we will get
+        from the config or deploy
+    Returns:
+        brownie.network.contract.ProjectContract: This is the most recently deployed
+        Contract of the type specified by a dictionary. This could either be a mock
+        or a 'real' contract on a live network.
+    """
+    # link_token
+    # LinkToken
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        contract = Contract.from_abi(
+            contract_type._name, contract_address, contract_type.abi
+        )
+    return contract
