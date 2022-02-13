9280dbc4f3584e2395ce7de9d5cfb8891298124e
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-13 13:36:12 +0900

    Advanced Collectible deploy scripts updated

--- scripts/advanced_collectible/deploy_and_create.py
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -1,10 +1,19 @@
-from scripts.helpful_scripts import get_account, OPENSEA_URL
+from scripts.helpful_scripts import get_account, OPENSEA_URL, get_contract
 from brownie import AdvancedCollectible
 
 
 def deploy_and_create():
     account = get_account()
-    advanced_collectible = AdvancedCollectible.deploy({"from": account})
+    # We want to be able to use the deployed contracts if we are on a testnet
+    # Otherwise, we want to deploy some mocks and use those
+    # Rinkeby
+    advanced_collectible = AdvancedCollectible.deploy(
+        get_contract("vrf_coordinator"),
+        get_contract("link_token"),
+        config["networks"][network.show_active()]["keyhash"],
+        config["networks"][network.show_active()]["fee"],
+        {"from": account},
+    )
 
 
 def main():
