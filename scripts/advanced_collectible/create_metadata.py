d24282ef0fa3d6dee66d68f8da003d3b6c54b249
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-17 21:21:42 +0900

    simple_metadata file created

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -1,4 +1,5 @@
 from brownie import AdvancedCollectible
+from scripts.helpful_scripts import get_gtype
 
 
 def main():
@@ -6,4 +7,4 @@ def main():
     number_of_advanced_collectibles = advanced_collectible.tokenCounter()
     print(f"You have created {number_of_advanced_collectibles} collectibles! ")
     for token_id in range(number_of_advanced_collectibles):
-        gtype = advanced_collectible.tokenIdToGodType(token_id)
+        gtype = get_gtype(advanced_collectible.tokenIdToGodType(token_id))
