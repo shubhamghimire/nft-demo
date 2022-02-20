acad56a95732f558e516f4875fabba7b148f5004
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-20 16:58:00 +0900

    IPFS upload successful

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -22,7 +22,8 @@ def main():
             collectible_metadata["name"] = gtype
             collectible_metadata["description"] = f"A mighty {gtype} god!"
             image_path = "./img/" + gtype.lower().replace("_", "-") + ".png"
-            # image_uri = upload_to_ipfs()
+            print(image_path)
+            image_uri = upload_to_ipfs(image_path)
             # collectible_metadata["image"] = image_uri
 
 
@@ -31,10 +32,13 @@ def upload_to_ipfs(filepath):
         image_binary = fp.read()
         ipfs_url = "http://127.0.0.1:5001"
         endpoint = "/api/v0/add"
-        response = requests.post(ipfs_url + endpoint, files="file":image_binary)
+        response = requests.post(ipfs_url + endpoint, files={"file": image_binary})
+        print(response.json())
         ipfs_hash = response.json()["Hash"]
-        # "./img/0-ganesh.png" --> "ganesh.png"
+        # "./img/0-SHIVA.png" -> "0-SHIVA.png"
         filename = filepath.split("/")[-1:][0]
+        print(filename)
+        print("filename printed")
         image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
         print(image_uri)
         return image_uri
